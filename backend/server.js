3b766d843db3beee99ad6be53c4bece8fe3e1978
Shahzad-Akbar <shahzadakbar823311@gmail.com>
2026-03-13 00:37:03 +0530

    url updated

--- backend/server.js
+++ backend/server.js
@@ -22,13 +22,20 @@ const PORT = process.env.PORT || 5000;
 // Update CORS for production
 const allowedOrigins = [
   process.env.FRONTEND_URL,
-  'http://localhost:3000',
-  'http://localhost:5000'
+  'https://applemedical.in',
+  'https://www.applemedical.in',
+  'http://localhost:3000'
 ];
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    if (!origin) return callback(null, true);
+    
+    const isAllowed = allowedOrigins.some(allowed => 
+      allowed && origin.startsWith(allowed.replace(/\/$/, "")))
+      || allowedOrigins.indexOf(origin) !== -1;
+
+    if (isAllowed) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
